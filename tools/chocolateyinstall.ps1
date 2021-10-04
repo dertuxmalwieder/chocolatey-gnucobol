7b9dd5d15c59752ce6e2227b0276788964da42b1
Cthulhux <>
2021-10-04 13:20:56 +0000

    Finally imported into VCS.
    FossilOrigin-Name: 4cbb20df2a5239ecfa4a5473440028789cdea1c3a3ea130d0bb30fbba1beea89

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName= 'GnuCOBOL'
+$distDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://arnoldtrembley.com/GC312-BDB-rename-7z-to-exe.7z'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $distDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'GnuCOBOL*'
+
+  checksum      = 'ED7CE3A65390490222501DBD94A1D8E2'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$files = Get-ChildItem $($distDir) -Include *.exe -Exclude cobc.exe,cobcrun.exe -Recurse
+foreach ($file in $files) {
+  # We only want to shim cobc and cobcrun.
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+Install-ChocolateyEnvironmentVariable -VariableName "COB_CONFIG_DIR" -VariableValue "$($distDir)\config"
+Install-ChocolateyEnvironmentVariable -VariableName "COB_CFLAGS" -VariableValue "-I`"$($distDir)\include`""
+Install-ChocolateyEnvironmentVariable -VariableName "COB_LIBRARY_PATH" -VariableValue "/LIBPATH:`"$($distDir)\lib`""
+Install-ChocolateyEnvironmentVariable -VariableName "COB_LIBS" -VariableValue "/LIBPATH:`"$($distDir)\lib\libcob.lib`""
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$uninstalled = $false
+
+# Clean the environment from our installed variables:
+Uninstall-ChocolateyEnvironmentVariable -VariableName "COB_CONFIG_DIR"
+Uninstall-ChocolateyEnvironmentVariable -VariableName "COB_CFLAGS"
+Uninstall-ChocolateyEnvironmentVariable -VariableName "COB_LIBRARY_PATH"
+Uninstall-ChocolateyEnvironmentVariable -VariableName "COB_LIBS"
